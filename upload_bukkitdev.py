978b5c374ef981b0dacf6dc891288449c5b89c4e
Esa Varemo <esa@kuivanto.fi>
2012-08-04 11:51:13 +0300

    Added more error checking

--- upload_bukkitdev.py
+++ upload_bukkitdev.py
@@ -14,24 +14,34 @@ from os import sep, makedirs
 from os.path import dirname, basename, exists, isdir, join
 from shutil import copy2
 
+#try:
+#    if argv[1].count(sep) < 3:
+#	print('Filename should be formated: pac/ka/ge/version/file')
+#	exit(-1)
+#    else: 
+#        path = argv[1]
+#except IndexError:
+#    print('Give a filename as first argument') 
+#    exit(-1)
+
 try:
-    if argv[1].count(sep) < 3:
-	print('Filename should be formated: pac/ka/ge/version/file')
-	exit(-1)
-    else: 
-        path = argv[1]
+    path = argv[1]
 except IndexError:
     print('Give a filename as first argument') 
     exit(-1)
+
+if not exists(path) or isdir(path):
+    print("File {path} does not exist\n".format(path=path))
+    exit(-1)
+
 
 #file = basename(path)
 #version = basename(dirname(path))
 #package = dirname(dirname(path))[2:].replace('/','.')
 
-
 pomfile = path.replace('.jar','.pom')
 
-if not exists(pomfile):
+if not exists(pomfile) or isdir(pomfile):
     print("Pomfile {pom} does not exist\n".format(pom=pomfile))
     exit(-1)
 
