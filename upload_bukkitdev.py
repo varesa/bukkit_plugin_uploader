7ae2c5a89509e2ac738b7c97e92bd42e2e061af2
Esa Varemo <esa@kuivanto.fi>
2012-08-04 11:52:06 +0300

    Removed old stuff

--- upload_bukkitdev.py
+++ upload_bukkitdev.py
@@ -14,15 +14,6 @@ from os import sep, makedirs
 from os.path import dirname, basename, exists, isdir, join
 from shutil import copy2
 
-#try:
-#    if argv[1].count(sep) < 3:
-#	print('Filename should be formated: pac/ka/ge/version/file')
-#	exit(-1)
-#    else: 
-#        path = argv[1]
-#except IndexError:
-#    print('Give a filename as first argument') 
-#    exit(-1)
 
 try:
     path = argv[1]
@@ -34,16 +25,13 @@ if not exists(path) or isdir(path):
     print("File {path} does not exist\n".format(path=path))
     exit(-1)
 
-
-#file = basename(path)
-#version = basename(dirname(path))
-#package = dirname(dirname(path))[2:].replace('/','.')
-
 pomfile = path.replace('.jar','.pom')
 
 if not exists(pomfile) or isdir(pomfile):
     print("Pomfile {pom} does not exist\n".format(pom=pomfile))
     exit(-1)
+
+
 
 pom = etree.parse(pomfile)
 pomroot = pom.getroot()
@@ -67,6 +55,8 @@ GroupID : {group}
 Artifact: {artifact}
 """.format(file=path, ver=version, pkg=package, group=groupid, artifact=artifact)
 print(string)
+
+
 
 
 if file == '' or version == '' or package == '':
